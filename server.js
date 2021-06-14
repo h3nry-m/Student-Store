b957f0ec1965c9837de0e364609b61ec07414d11
rrll3553 <liveoak35@gmail.com>
2021-06-14 16:36:58 -0700

    Initialized project and ignored node_modules

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const morgan = require("morgan")
+
+const app = express()
+
+app.use(morgan("tiny"))
+app.get("/", async (req, res, next) => {
+  res.status(200).json({ping: "pong"})
+})
+
+const port = 3000
+
+app.listen(port, ()=> {
+  console.log(`🚀 Server listening on port ` + port)
+})
+
